de95b106e35e805250197dd56da4ab02a04ff251
helihui <helihui_108699@163.com>
2020-12-11 00:02:59 +0800

    底部导航更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
+
+// 引入路由模块化文件
+import centerRouter from './routes/center'
+import cinemaRouter from './routes/cinema' 
+import filmRouter from './routes/film'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    // 访问跟路由，进行重定向
+    redirect: '/film'
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  centerRouter,
+  cinemaRouter,
+  filmRouter
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+// base: process.env.BASE_URL,
   routes
 })
 
